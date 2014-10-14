138c544bdf2da142f67b1c39f826d1794abcc8a2
christran206 <christran206@gmail.com>
2014-10-14 13:06:05 -0700

    Visual Studio Project to acquire facial data

---
+++ CSS490Kinect/CSS490Kinect/MainWindow.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+
+namespace CSS490Kinect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Coordinate mapper to map one type of point to another
+        /// </summary>
+        private CoordinateMapper coordinateMapper = null;
+
+        /// <summary>
+        /// Reader for body frames
+        /// </summary>
+        private BodyFrameReader bodyFrameReader = null;
+
+        /// <summary>
+        /// Array to store bodies
+        /// </summary>
+        private Body[] bodies = null;
+
+        /// <summary>
+        /// Number of bodies tracked
+        /// </summary>
+        private int bodyCount;
+
+        /// <summary>
+        /// Face frame sources
+        /// </summary>
+        private FaceFrameSource[] faceFrameSources = null;
+
+        /// <summary>
+        /// Face frame readers
+        /// </summary>
+        private FaceFrameReader[] faceFrameReaders = null;
+
+        /// <summary>
+        /// Storage for face frame results
+        /// </summary>
+        private FaceFrameResult[] faceFrameResults = null;
+
+        public MainWindow()
+        {
+            // one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // get the coordinate mapper
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+            // get the color frame details
+            FrameDescription frameDescription = this.kinectSensor.ColorFrameSource.FrameDescription;
+
+            // open the reader for the body frames
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+
+            // wire handler for body frame arrival
+            this.bodyFrameReader.FrameArrived += this.Reader_BodyFrameArrived;
+
+            // set the maximum number of bodies that would be tracked by Kinect
+            this.bodyCount = this.kinectSensor.BodyFrameSource.BodyCount;
+
+            // allocate storage to store body objects
+            this.bodies = new Body[this.bodyCount];
+
+            // specify the required face frame results
+            FaceFrameFeatures faceFrameFeatures =
+                FaceFrameFeatures.FaceEngagement
+                | FaceFrameFeatures.Glasses
+                | FaceFrameFeatures.LeftEyeClosed
+                | FaceFrameFeatures.RightEyeClosed
+                | FaceFrameFeatures.LookingAway;
+
+            // create a face frame source + reader to track each face in the FOV
+            this.faceFrameSources = new FaceFrameSource[this.bodyCount];
+            this.faceFrameReaders = new FaceFrameReader[this.bodyCount];
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                // create the face frame source with the required face frame features and an initial tracking Id of 0
+                this.faceFrameSources[i] = new FaceFrameSource(this.kinectSensor, 0, faceFrameFeatures);
+
+                // open the corresponding reader
+                this.faceFrameReaders[i] = this.faceFrameSources[i].OpenReader();
+            }
+
+            // allocate storage to store face frame results for each face in the FOV
+            this.faceFrameResults = new FaceFrameResult[this.bodyCount];
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            // initialize the components (controls) of the window
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Handles the body frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_BodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    int bodiesTracked = 0;
+                    int facesTracked = 0;
+                    // update body data
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+
+                    // iterate through each face source
+                    for (int i = 0; i < this.bodyCount; i++)
+                    {
+                        if (this.bodies[i].IsTracked)
+                        {
+                            bodiesTracked++;
+                            // update the face frame source to track this body
+                            this.faceFrameSources[i].TrackingId = this.bodies[i].TrackingId;
+                            
+                            // check if a valid face is tracked in this face source
+                            if (this.faceFrameSources[i].IsTrackingIdValid)
+                            {
+                                facesTracked++;
+                            }
+                        }
+
+                    }
+
+                    BodyTrackedText.Text = "Bodies Tracked: " + bodiesTracked;
+                    FacesTrackedText.Text = "Faces Tracked: " + facesTracked;
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                if (this.faceFrameReaders[i] != null)
+                {
+                    // wire handler for face frame arrival
+                    this.faceFrameReaders[i].FrameArrived += this.Reader_FaceFrameArrived;
+                }
+            }
+
+            if (this.bodyFrameReader != null)
+            {
+                // wire handler for body frame arrival
+                this.bodyFrameReader.FrameArrived += this.Reader_BodyFrameArrived;
+            }
+        }
+
+        /// <summary>
+        /// Handles the face frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_FaceFrameArrived(object sender, FaceFrameArrivedEventArgs e)
+        {
+            using (FaceFrame faceFrame = e.FrameReference.AcquireFrame())
+            {
+                if (faceFrame != null)
+                {
+                    int index = this.GetFaceSourceIndex(faceFrame.FaceFrameSource);
+                    this.faceFrameResults[index] = faceFrame.FaceFrameResult;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the index of the face frame source
+        /// </summary>
+        /// <param name="faceFrameSource">the face frame source</param>
+        /// <returns>the index of the face source in the face source array</returns>
+        private int GetFaceSourceIndex(FaceFrameSource faceFrameSource)
+        {
+            int index = -1;
+
+            for (int i = 0; i < this.bodyCount; i++)
+            {
+                if (this.faceFrameSources[i] == faceFrameSource)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        private void UpdateFaceStats()
+        {
+            String statusText = "";
+            for (int i = 0; i < bodyCount; i++)
+            {
+                DetectionResult faceEngauged;
+                faceFrameResults[i].FaceProperties.TryGetValue(FaceProperty.Engaged, out faceEngauged);
+                statusText += "Face of Body " + i + faceEngauged.ToString() + "/n";
+            }
+
+            bodyInfoText.Text = statusText;
+        }
+    }
+}
---
+++ CSS490Kinect/CSS490Kinect/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSS490Kinect
+{
+    class People
+    {
+        public bool EyesOpen { get; private set; }
+        public bool Engauged { get; private set;}
+
+        public People (bool engauged, bool eyesOpen) {
+            this.Engauged = engauged;
+            this.EyesOpen = eyesOpen;
+        }
+    }
+}
